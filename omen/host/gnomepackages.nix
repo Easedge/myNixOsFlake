d29c4640a5b96ae6314945feb8ef65712fc8001a
rodney sgl chang <zhangrundide@gmail.com>
2023-06-02 22:40:55 +0800

    Update gnomepackages.nix

--- omen/host/gnomepackages.nix
+++ omen/host/gnomepackages.nix
@@ -1,6 +1,9 @@
 { lib, config, pkgs, ... }:
 
 {
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  
   environment.systemPackages = with pkgs; [
     gnome.pomodoro
     gnomeExtensions.pano
