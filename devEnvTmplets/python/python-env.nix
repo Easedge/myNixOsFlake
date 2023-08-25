09222e07217ad8cd4613b75fafbdad4cb7f00136
rodney sgl chang <zhangrundide@gmail.com>
2023-08-26 02:23:56 +0800

    Delete devEnvTmplets/python directory

--- devEnvTmplets/python/python-env.nix
+++
@@ -1,5 +0,0 @@
-with import <nixpkgs> { };
-
-python3.withPackages (ps: with ps; [
-  # xxx
-])
--- devEnvTmplets/python/python-shell.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-let
-  pythonEnv = pkgs.python3.withPackages (ps: [
-    # ps.xxx
-  ]);
-in
-pythonEnv.env
