7f187b3ab8c3dfd12c9678dcfb72f4559dba99a4
Rodney Chang <zhangrundide@gmail.com>
2023-08-29 00:15:54 +0800

    update hardware

--- omen/host/hardware-configuration.nix
+++ omen/host/hardware-configuration.nix
@@ -35,8 +35,12 @@
   networking.interfaces.eno1.useDHCP = lib.mkDefault true;
   networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
+  zramSwap.enable = true;
+  services.zram-generator.enable = true;
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.opengl.enable = true;
+  hardware.steam-hardware.enable = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
