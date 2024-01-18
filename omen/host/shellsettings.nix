3e7e29f5e2c53002a3cbf5563b17832572dadd50
Rodney Chang <zhangrundide@gmail.com>
2024-01-18 16:36:38 +0800

    update bash prompt

--- omen/host/shellsettings.nix
+++ omen/host/shellsettings.nix
@@ -20,7 +20,18 @@
     shellInit = ''
       eval "$(direnv hook bash)"
     '';
-    promptInit = "";
+    promptInit = ''
+      if [ -n "$BASH_VERSION" ]; then
+        export PS1='\[\e[38;5;135m\]\u\[\e[0m\]@\[\e[38;5;166m\]\h\[\e[0m\] \[\e[38;5;118m\]\w\[\e[0m\] \$ '
+      else
+        if [ "$UID" -eq 0 ]; then
+          export PROMPT="%F{135}%n%f@%F{166}%m%f %F{118}%~%f %# "
+        else
+          export PROMPT="%F{135}%n%f@%F{166}%m%f %F{118}%~%f \$ "
+        fi
+      fi
+      export PS1="\[\e]0;\u@\h: \w\a\]$PS1"
+    '';
     shellAliases = {
       emc = "emacsclient";
     };
